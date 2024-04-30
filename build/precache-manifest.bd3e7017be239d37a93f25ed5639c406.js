824451dd890d04dd2388bc69c8b2c395f30e8832
Golf <g_peerapat@planforfit.com>
2024-04-30 15:09:57 +0700

    เปลี่ยน package.json

---
+++ build/precache-manifest.bd3e7017be239d37a93f25ed5639c406.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a047f0efdc9b5da34e8cbdb95cbd03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be7f76221fc538e9ebb5",
+    "url": "/static/css/2.7f2699be.chunk.css"
+  },
+  {
+    "revision": "63a4ff200d39af1c1ef1",
+    "url": "/static/css/main.fb4dd321.chunk.css"
+  },
+  {
+    "revision": "be7f76221fc538e9ebb5",
+    "url": "/static/js/2.575a0bbb.chunk.js"
+  },
+  {
+    "revision": "f1e65c43bbe9a18bf7d7516992993758",
+    "url": "/static/js/2.575a0bbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a4ff200d39af1c1ef1",
+    "url": "/static/js/main.20f9be17.chunk.js"
+  },
+  {
+    "revision": "e3daf7b03316b39ebd46",
+    "url": "/static/js/runtime-main.724286bc.js"
+  },
+  {
+    "revision": "14e48cb23de970fb46c9462e573d5794",
+    "url": "/static/media/banner_login.14e48cb2.png"
+  },
+  {
+    "revision": "b6831ec74260a352c1d23a4f09a658e3",
+    "url": "/static/media/bg_login.b6831ec7.png"
+  },
+  {
+    "revision": "927466beb51bfcec92907d5163cd4833",
+    "url": "/static/media/brave&burn1.927466be.png"
+  },
+  {
+    "revision": "2c70e9963496268158f8673bb74d2007",
+    "url": "/static/media/brave&burn2.2c70e996.png"
+  },
+  {
+    "revision": "3ea850de83517d0dc98379fe79383e3a",
+    "url": "/static/media/brave&burn3.3ea850de.png"
+  },
+  {
+    "revision": "abde6edaf5154dd8321b846830d8a71c",
+    "url": "/static/media/brave&burn4.abde6eda.png"
+  },
+  {
+    "revision": "f598899d191963ab2f21c54c2efae981",
+    "url": "/static/media/brave&burn5.f598899d.png"
+  },
+  {
+    "revision": "136226f5a7a2131e970c59a8081ddc8d",
+    "url": "/static/media/brave&burn6.136226f5.png"
+  },
+  {
+    "revision": "bfaa5b4a3d79ef03c21862379685bfd8",
+    "url": "/static/media/brave&burn7.bfaa5b4a.png"
+  },
+  {
+    "revision": "d1dd40407ca50d8313dc0205d32fa066",
+    "url": "/static/media/brave&burn8.d1dd4040.png"
+  },
+  {
+    "revision": "61162a9c708d4cfb86ef3bb2d1860075",
+    "url": "/static/media/brave&burn_complete1.61162a9c.png"
+  },
+  {
+    "revision": "4d04f658ca77200259131b24419ad1c3",
+    "url": "/static/media/brave&burn_complete2.4d04f658.png"
+  },
+  {
+    "revision": "c72404c8b01003a3ad61d2290109c3b7",
+    "url": "/static/media/brave&burn_complete3.c72404c8.png"
+  },
+  {
+    "revision": "72cc89b4373badf9f29ae481ecd6b752",
+    "url": "/static/media/brave&burn_complete4.72cc89b4.png"
+  },
+  {
+    "revision": "e1e66b038bdea28d17721287a3c9329a",
+    "url": "/static/media/brave&burn_complete5.e1e66b03.png"
+  },
+  {
+    "revision": "d309355b134ba0a55ff58cd415aebbfa",
+    "url": "/static/media/brave&burn_complete6.d309355b.png"
+  },
+  {
+    "revision": "89aabf85731a51e487e7c5d86b747ab7",
+    "url": "/static/media/brave&burn_complete7.89aabf85.png"
+  },
+  {
+    "revision": "436afa5d3a185456e80ef967fa5c258a",
+    "url": "/static/media/brave&burn_complete8.436afa5d.png"
+  },
+  {
+    "revision": "de0c9d8fad60233395b2e5ea8d67b3af",
+    "url": "/static/media/frame40.de0c9d8f.png"
+  },
+  {
+    "revision": "18ddd45f567d97e98c2d9286e87094ee",
+    "url": "/static/media/frame41.18ddd45f.png"
+  },
+  {
+    "revision": "19262d5b4229158be98f2fb2e68e118b",
+    "url": "/static/media/frame42.19262d5b.png"
+  },
+  {
+    "revision": "ec86641804f5a5fd8399729cf1a98b30",
+    "url": "/static/media/frame43.ec866418.png"
+  },
+  {
+    "revision": "ae3c596e19710993831d331017f58800",
+    "url": "/static/media/frame44.ae3c596e.png"
+  },
+  {
+    "revision": "53f8b181200a3d5a35307880738fa608",
+    "url": "/static/media/frame45.53f8b181.png"
+  },
+  {
+    "revision": "f30aa70201da67f2d5a8152524494e77",
+    "url": "/static/media/frame46.f30aa702.png"
+  },
+  {
+    "revision": "ecb5729c0a26f852ed576493ff48e83f",
+    "url": "/static/media/frame47.ecb5729c.png"
+  },
+  {
+    "revision": "5afadbaa983fc7b2c51467e305541df9",
+    "url": "/static/media/header_preem.5afadbaa.png"
+  },
+  {
+    "revision": "6335ce4944055ad27e513171f2b961c3",
+    "url": "/static/media/icon_Nutrition.6335ce49.png"
+  },
+  {
+    "revision": "bc7b116419e176b2025a34ac7222b620",
+    "url": "/static/media/icon_protein.bc7b1164.png"
+  },
+  {
+    "revision": "0f06707ccc61e2a750d22ec9c3c95db0",
+    "url": "/static/media/mainbg.0f06707c.jpg"
+  },
+  {
+    "revision": "4612b6c0a0e649719860f0a12ba65849",
+    "url": "/static/media/modal.4612b6c0.jpg"
+  }
+]);
